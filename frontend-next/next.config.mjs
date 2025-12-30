0844fb950b457344b656368ed0913358c1612125
Paulo.enc <phdsde2@gmail.com>
2025-12-30 12:42:54 -0300

    ajuste de rota upload

--- frontend-next/next.config.mjs
+++ frontend-next/next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+    // Aumenta o limite de tamanho do corpo da requisição
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '10mb', // Pode aumentar para '20mb' se precisar
+        },
+    },
+    // Adicione cabeçalhos para evitar problemas de CORS com arquivos grandes
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
